2fe55df250785596b8442e9851a5d29907cc81a5
Your Name <you@example.com>
2024-08-03 03:38:21 +0300

    new update

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ url = 'https://rvist.ac.ke/'
 page = urlopen(url)
 html = page.read().decode('utf-8')
 soup = BeautifulSoup(html, "html.parser")
-
+print(html)
 print(soup.get_text())
 print(soup.title.string)
