69e1144ef97a15bdb5ac3350b6d5a255f6759f0b
Your Name <you@example.com>
2024-08-05 14:44:12 +0300

    update

--- url_test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-url = 'https://rvist.ac.ke/hostel-accommodation/'
-page = urlopen(url)
-html = page.read().decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-print(html)
