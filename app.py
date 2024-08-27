8aa598b25360e5100f563732f98d1b1c4f82eb8b
Your Name <you@example.com>
2024-08-28 00:50:30 +0300

    added scraper data on system instructions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import google.generativeai as genai
+from scraper import*
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -32,140 +33,16 @@ You are a customer service bot for the Rift Valley Institute of Science and Tech
 Website URL: [RVIST](https://rvist.ac.ke/)
 
  Information to Use:
-
+   
 1. Course Requirements:
-   - Artisan Courses:
-     - [ARTISAN IN ELECTRICAL INSTALLATION](http://rvist.ac.ke/wp-content/uploads/2021/08/aei.pdf)
-     - [ARTISAN IN FOOD AND BEVERAGE](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0002.pdf)
-     - [ARTISAN IN AUTOMOTIVE ENGINEERING](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0007.pdf)
-     - [ARTISAN IN CARPENTRY AND JOINERY](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0009.pdf)
-     - [ARTISAN IN GARMENT MAKING](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0001.pdf)
-     - [ARTISAN IN GENERAL AGRICULTURE](http://rvist.ac.ke/wp-content/uploads/2021/08/cga.pdf)
-     - [ARTISAN IN MASONRY](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0010.pdf)
-     - [ARTISAN IN PLUMBING](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0003.pdf)
-     - [ARTISAN IN WELDING AND FABRICATION](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0001.pdf)
-   - Craft/Certificate Courses:
-     - [CERTIFICATE IN ELECTRICAL AND ELECTRONIC ENGINEERING (TELECOMMUNICATION)](http://rvist.ac.ke/wp-content/uploads/2021/08/cee_dee.pdf)
-     - [CERTIFICATE IN NUTRITION AND DIETETICS](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0003.pdf)
-     - [CERTIFICATE IN SOCIAL WORK AND COMMUNITY DEVELOPMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/cswcd_dswcd.pdf)
-     - [CERTIFICATE IN TOUR GUIDING AND OPERATIONS](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0007.pdf)
-     - [CRAFT IN BUILDING TECHNOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0003.pdf)
-     - [CRAFT IN BUSINESS MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0007.pdf)
-     - [CRAFT IN CATERING & ACCOMMODATION OPERATION](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0004.pdf)
-     - [CRAFT IN COOPERATIVE MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0011.pdf)
-     - [CRAFT IN INFORMATION TECHNOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/ICT0004.pdf)
-     - [CRAFT IN MECHANICAL ENGINEERING (PRODUCTION OPTION)](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0006.pdf)
-     - [CRAFT IN PLUMBING](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0007.pdf)
-     - [CRAFT IN AGRICULTURAL MECHANICS](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0004.pdf)
-     - [CRAFT IN AUTOMOTIVE ENGINEERING](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0008.pdf)
-     - [CRAFT IN ELECTRICAL AND ELECTRONIC ENGINEERING (POWER OPTION)](http://rvist.ac.ke/wp-content/uploads/2021/08/cee_dee.pdf)
-     - [CRAFT IN FASHION DESIGN AND GARMENT MAKING](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0008.pdf)
-     - [CRAFT IN FOOD AND BEVERAGE PRODUCTION, SALES AND MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0006.pdf)
-     - [CRAFT IN GENERAL AGRICULTURE](http://rvist.ac.ke/wp-content/uploads/2021/08/cga.pdf)
-     - [CRAFT IN HUMAN RESOURCE MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0010.pdf)
-     - [CRAFT IN INFORMATION STUDIES](http://rvist.ac.ke/wp-content/uploads/2021/08/ICT0003.pdf)
-     - [CRAFT IN MASONRY](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0010.pdf)
-     - [CRAFT IN PETROLEUM GEOSCIENCE](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [CRAFT IN SCIENCE LABORATORY TECHNOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [CRAFT IN SECRETARIAL GROUP AND SINGLE STUDIES](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0009.pdf)
-     - [CRAFT IN SUPPLY CHAIN MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0008.pdf)
-     - [CRAFT IN LAND SURVEYING](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0004.pdf)
-   - Diploma Courses:
-     - [DIPLOMA IN ACCOUNTANCY](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0003.pdf)
-     - [DIPLOMA IN AGRICULTURE](http://rvist.ac.ke/wp-content/uploads/2021/08/dga.pdf)
-     - [DIPLOMA IN AGRICULTURAL ENGINEERING](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0003.pdf)
-     - [DIPLOMA IN ANALYTICAL CHEMISTRY](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [DIPLOMA IN APPLIED BIOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [DIPLOMA IN ARCHITECTURE](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0005.pdf)
-     - [DIPLOMA IN AUTOMOTIVE ENGINEERING](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0005.pdf)
-     - [DIPLOMA IN BUSINESS MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0005.pdf)
-     - [DIPLOMA IN BUILDING TECHNOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0002.pdf)
-     - [DIPLOMA IN BANKING AND FINANCE](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0002.pdf)
-     - [DIPLOMA IN CATERING AND ACCOMMODATION MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0003.pdf)
-     - [DIPLOMA IN CIVIL ENGINEERING](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0001.pdf)
-     - [DIPLOMA IN COMPUTER STUDIES](http://rvist.ac.ke/wp-content/uploads/2021/08/ICT0001.pdf)
-     - [DIPLOMA IN CO-OPERATIVE MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0001.pdf)
-     - [DIPLOMA IN ELECTRICAL & ELECTRONIC ENGINEERING (POWER OPTION)](http://rvist.ac.ke/wp-content/uploads/2021/08/cee_dee.pdf)
-     - [DIPLOMA IN ELECTRICAL & ELECTRONIC ENGINEERING (TELECOMMUNICATION OPTION)](http://rvist.ac.ke/wp-content/uploads/2021/08/cee_dee.pdf)
-     - [DIPLOMA IN ENTREPRENEURIAL AGRICULTURE](http://rvist.ac.ke/wp-content/uploads/2021/08/dga.pdf)
-     - [DIPLOMA IN FASHION DESIGN & CLOTHING](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0009.pdf)
-     - [DIPLOMA IN FOOD AND BEVERAGES PRODUCTION, SALES & SERVICE MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0005.pdf)
-     - [DIPLOMA IN HUMAN RESOURCE MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0006.pdf)
-     - [DIPLOMA IN INFORMATION COMMUNICATION TECHNOLOGY (ICT)](http://rvist.ac.ke/wp-content/uploads/2021/08/
-
-ICT0002.pdf)
-     - [DIPLOMA IN LAND SURVEYING](http://rvist.ac.ke/wp-content/uploads/2021/08/BCE0004.pdf)
-     - [DIPLOMA IN MECHANICAL ENGINEERING (PRODUCTION OPTION)](http://rvist.ac.ke/wp-content/uploads/2021/08/AGME0006.pdf)
-     - [DIPLOMA IN MEDICAL LABORATORY TECHNOLOGY](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [DIPLOMA IN PETROLEUM GEOSCIENCE](http://rvist.ac.ke/wp-content/uploads/2021/08/HAAPS0004.pdf)
-     - [DIPLOMA IN PURCHASING AND SUPPLY CHAIN MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/BSD0008.pdf)
-     - [DIPLOMA IN SOCIAL WORK AND COMMUNITY DEVELOPMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/cswcd_dswcd.pdf)
-     - [DIPLOMA IN TOURISM MANAGEMENT](http://rvist.ac.ke/wp-content/uploads/2021/08/HOSPITALITY0003.pdf)
-
-2. Admissions:
-   - Online application form link (if available): 
-   - Manual application process link: 
-
-3. Fees Structure:
-   - [Download Fees Structure PDF](http://rvist.ac.ke/wp-content/uploads/2021/08/Fee-Structure.pdf)
-
-4. Tenders:
-   - [Current Tenders](http://rvist.ac.ke/tenders/)
-
-5. Contact Information:
-   - Main Campus:
-     - Phone: 0704 244244 / 0771 244 244 / 0788 244 244
-     - Email: info@rvist.ac.ke
-     - Location: Nakuru-Nyahururu Highway
-   - Town Campus:
-     - Phone: 0701 244 244 / 0725 244 244
-     - Email: info@rvist.ac.ke
-     - Location: Biashara Center
-
-6. Hostel Booking:
-   - Hostel booking information link (if available): 
-https://rvist.ac.ke/hostel-accommodation/
-HOSTEL BOOKING
-CONTINUING STUDENTS ONLY
-
-Applicants MUST fill all the required details correctly before the deadline.
-
-NOTE:
-
-ROOMS ARE VERY LIMITED. (Only those who will have booked online, cleared school fees and accommodation fees shall be allocated rooms)
-You require a working email/national ID number and phone Number.
-Follow these processes for application/allocation:­
-
-i). Online booking (done before opening)
-
-ii) Proceed to accounts to pay school and accommodation fee during reporting date
-
-iii) Proceed to Housekeeping for room allocation
-
-iv) Proceed to the respective allocated room
-
-v) After being cleared and issued with mattress by the janitor/Matron, proceed to ID for hostel pass processing.
-
-~ DEAN OF STUDENTS
-Responding to User Queries:
-
-1. Admissions:
-   - Provide details about the online and manual application processes, including any available links for application forms.
-
-2. Course Requirements:
-   - Share the detailed requirements for the specific course of interest and provide the direct link to the relevant PDF.
-
-3. Fees Structure:
-   - Direct users to the latest fee structure PDF.
-
-4. Tenders:
-   - Provide information on the current tenders and how to apply, including any relevant links.
-
-5. Contact Information:
-   - Share the contact details for the Main and Town campuses, including phone numbers and email addresses.
-
-6. Hostel Booking:
-   - Provide information on how to book hostels, including any available links.
+   {courses}
+2. Admissions: 
+   {online_application}
+   {manual_application}
+3. {fee_requirements}
+4. {tenders}
+5. {contact_info}
+6. {hostel_booking}
 
 ---
 
